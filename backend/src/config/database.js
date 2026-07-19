e32ec251c8d810735dec72f7f024c0ffab86ebe3
Kevin Ulrich <kevin.ulrich.free@gmail.com>
2026-07-19 18:10:50 +0200

    Audit backend et finalisation des services

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,13 +1,6 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-console.log({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
-});
-
 const database = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
